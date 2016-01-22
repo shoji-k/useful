947a02fb2db2184517bf7b721d507c1057eb60c1
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-22 13:47:32 +0900

    update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,9 +9,20 @@ Dropbox-Uploader/dropbox_uploader.sh upload backup/$TODAY.dbbackup sample.dbback
 MONTHAGO=`date -d '31 days ago' '+%F'`
 rm -f backup/$MONTHAGO.dbbackup
 
+user_name="user"
 fileinfo=`ls -lh backup/$TODAY.dbbackup | awk '{print $5}'`
-curl_content='payload={"channel": "#backup", "username": "sample", "icon_emoji": ":smile:", "text": "'
-curl_content="${curl_content} ${TODAY}.dbbackup ${fileinfo} backup done"
+if [ -n "$fileinfo" ] ; then
+  icon=":wrench:"
+  text="${TODAY}.dbbackup ${fileinfo} backup done"
+else
+  icon=":rage:"
+  text="${TODAY}.dbbackup backup FAIL!!!"
+fi
+
+curl_content='payload={"channel": "#backup", "username": "'${user_name}'",'
+curl_content=${curl_content}' "icon_emoji": "'${icon}'", "text": "'
+curl_content="${curl_content} ${text}"
 curl_content="${curl_content}\"}"
-curl="curl -X POST --data-urlencode `echo -e "'${curl_content}'"` https://hooks.slack.com/services/xxxxxxxxxxxx"
+curl="curl -X POST --data-urlencode `echo -e "'${curl_content}'"` https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXX"
 eval $curl
+
