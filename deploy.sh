9580e30c928d07f6d9aee5c324906f7b22d46f7e
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-26 18:06:32 +0900

    add exclucde rule to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -C --exclude="/sql" --exclude="/*.sh" --exclude="/composer*" --filter=":- .gitignore" -rltuvz ./ sample_ssh_host:~/www/
+rsync -C --exclude="/sql" --exclude="/*.sh" --exclude="/composer*" --exclude=".gitignore" --filter=":- .gitignore" -rltuvz ./ sample_ssh_host:~/www/
