5d943a9d70c08b8f8837bc7e4ec11705e09b8b7d
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-03-16 10:24:16 +0900

    add ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,21 @@
+!#/bin/sh
+
+# upload id_rsa.pub to ~/
+
+cd ~
+mkdir .ssh
+touch .ssh/authorized_keys
+cat id_rsa.pub > .ssh/authorized_keys
+rm id_rsa.pub
+chmod 600 .ssh/authorized_keys
+
+
+# $ vi /etc/ssh/sshd_config
+# ```
+# Host *
+# Port 20022
+# Protocol 2
+# PermitRootLogin without-password
+# ```
+
+sudo service ssh restart
