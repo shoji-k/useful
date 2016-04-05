6ac358ff74b31a375026808fe1eb6326ea867df1
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-04-05 19:04:51 +0900

    install nvm after ansible installing

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ ssh-keygen -t rsa -b 4096 -C "sample@sample.com"
 # create id_rsa and id_rsa.pub
 # copy id_rsa.pub text to each services
 #   github.com or bitbucket.com ..
+
+# after ansible implementation
+
+nvm ls
+
+# check version of node
+
+nvm install v5.10.0
