94aa3d267d58224d251859bcf30444203acdc534
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-04-09 19:07:57 +0900

    update starting script

--- ansible.sh
+++ ansible.sh
@@ -4,3 +4,5 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+
+# next> ssh.sh
--- ssh.sh
+++ ssh.sh
@@ -9,13 +9,14 @@ cat id_rsa.pub > .ssh/authorized_keys
 rm id_rsa.pub
 chmod 600 .ssh/authorized_keys
 
-
+# change ssh setting
 # $ vi /etc/ssh/sshd_config
 # ```
 # Host *
 # Port 20022
 # Protocol 2
 # PermitRootLogin without-password
+# PasswordAuthentication no
 # ```
 
 sudo service ssh restart
